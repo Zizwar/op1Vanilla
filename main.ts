fdc17dcdda955b0e6fa3012d3f76d0a6397726e1
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2024-07-13 01:48:31 +0100

    Update main.rs for serv

---
+++ main.ts
@@ -0,0 +1,18 @@
+//if deno
+//import { Hono } from "npm:hono";
+
+//if nodejs
+//import { Hono } from "hono";
+
+import { Hono } from "https://deno.land/x/hono/mod.ts";
+import {
+  serveStatic
+} from "https://deno.land/x/hono/middleware.ts";
+
+
+
+const app = new Hono();
+
+app.all("*", serveStatic({ root: "./vanilla" }));
+//
+Deno.serve(app.fetch);
